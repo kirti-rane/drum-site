424196c1782a05ee8b024ddbf95aa59477ce013b
Kirti Rane <kirtirane@Kirtis-MacBook-Pro.local>
2023-05-08 12:27:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+var maxlen = document.querySelectorAll(".drum").length;
+
+for(var i =0; i<maxlen; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", myfunction);
+
+}
+function myfunction(){
+
+    var buttonPressed = this.innerHTML;
+    makeSound(buttonPressed);
+    buttonAnime(buttonPressed)
+
+}
+
+document.addEventListener('keydown', function(event){
+
+    makeSound(event.key);
+    buttonAnime(event.key)
+})
+
+function makeSound(key){
+
+    switch(key){
+
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+    }
+
+}
+
+function buttonAnime(individualClass){
+
+    document.querySelector("." + individualClass).classList.add("pressed");
+
+    setTimeout(function(){
+
+        document.querySelector("." + individualClass).classList.remove("pressed")
+    }, 100)
+}
+
+
+
+
+
+
